378339438a6d47b1f90cc57c34054e954ece1601
Saeda Nizam <41684858+saeeda-nizam@users.noreply.github.com>
2020-06-03 16:35:04 +0200

    init: summiereZahlenEinesBereiche passed
    	Programm laüft . erledigt

--- cpp-basics/Schleifen/SummiereZahlenEinesBereiches/main.cpp
+++ cpp-basics/Schleifen/SummiereZahlenEinesBereiches/main.cpp
@@ -2,8 +2,45 @@
 
 using namespace std;
 
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    int summegrenzenZahl1;
+    int summegrenzenZahl2;
+    int summeBerechnen;
+    int x;
+
+
+    cout << " summeBerechnen " <<endl;
+    cout << " von " <<endl;
+    cin >> summegrenzenZahl1;
+    cout << " bis " <<endl;
+    cin >>summegrenzenZahl2;
+
+    summeBerechnen=summegrenzenZahl1;
+
+
+    for (int i=summegrenzenZahl1+1; i<=summegrenzenZahl1 ; i++) {
+
+        summeBerechnen += i;
+
+    }
+
+    cout << " =  " << summeBerechnen << endl;
+
+    //teil2
+
+    summeBerechnen=summegrenzenZahl1;
+
+    cout << summegrenzenZahl1;
+
+    for (x=summegrenzenZahl1+1; x<=summegrenzenZahl2; x++) {
+        cout << " + " << x;
+        summeBerechnen += x;
+    }
+    cout << " = " << summeBerechnen << endl << endl;
+
     return 0;
-}
+    }
