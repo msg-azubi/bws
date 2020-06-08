030be506dcabf9f6f6a91783d1cf2337713d6b38
robischp <Peter.Robisch@msg.group>
2020-06-08 22:27:45 +0200

    Der erste Test definiert die zubenutztende API.
        Zeugnis::berechneDurchschnitt(noten)
    
    Der Testcode wird von zwei Direktiven umschlossen:
        #ifndef TST_ZEUGNIS_H
          // Testing
          // Code der vom Test-Framework gelesen wird
        #endif // TST_ZEUGNIS_H
    
    Damit die obige API benutzt werden kann,
    braucht es entsprechen Definitionen.
    Diese Definitionen schreiben wir direkt in Datei
    
    	`tst_zeugnis.h`
    und zwar vor die Direktive
    	`#ifndef TST_ZEUGNIS_H`.
    
    Die Definition des Namespace
    	`Zeugnis`
    enthält die Definition zu der Funktionsdeklaration:
    	`int berechneDurchschnitt(int noten[])`
    
    Aktuell wollen wir nur die Möglichkeit haben,
    einen Test laufen zu lassen. Dazu muss es eine API
    geben, die der Test benutzt.
    
    Die Signature des Test ist
    	Test(ArrayExample,
    	     ZeugnisDurchschnitt_ist_glatte_Note)
    Der Test enthält eine Prüfung, die die obige API benutzt:
    	EXPECT_EQ(
    		Zeugnis::berechneDurchschnitt(noten),
    		erwarteterDurchschnitt) ;
    
    Zum jetztigen Zeitpunkt ist es völlig in Ordnung,
    wenn der Test beim Laufenlassen "failed",
    also fehlschlägt.
    
    TDD heißt:
    - Test schreiben, d.h API schreiben
    - Test laufen lassen, d.h prüfen, ob API compiliert
    - Produktionscode schreiben, d.h. API implementieren
    - Test laufen, d.h prüfen, ob Test "passed",
      also der Test erfolgreich passiert wird.

--- cpp-basics/Arrays/ZeugnisTest/tst_zeugnis.h
+++ cpp-basics/Arrays/ZeugnisTest/tst_zeugnis.h
@@ -1,15 +1,32 @@
+namespace Zeugnis {
+
+    int berechneDurchschnitt(int noten[]){
+        return 6;   // Aktuell geht es darum, dass die API erfüllt wird.
+                    // Die Deklaration sagt, das eine Zahl von Typ `int`
+                    // als Rückgabewert erwartet wird.
+    }
+
+}
+
 #ifndef TST_ZEUGNIS_H
 #define TST_ZEUGNIS_H
 
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 
+
 using namespace testing;
 
-TEST(ArrayExample, Zeugnis)
+TEST(ArrayExample, ZeugnisDurchschnitt_ist_glatte_Note)
 {
-    EXPECT_EQ(1, 1);
-    ASSERT_THAT(0, Eq(0));
+    // Diese Noten von 4 Fächern
+    int noten[] { 2, 2, 4, 4 };
+
+    // haben einen Durchschnitt
+    int erwarteterDurchschnitt = 3;
+
+    // Der Test prüft:
+    ASSERT_EQ(Zeugnis::berechneDurchschnitt(noten), erwarteterDurchschnitt) ;
 }
 
 #endif // TST_ZEUGNIS_H
