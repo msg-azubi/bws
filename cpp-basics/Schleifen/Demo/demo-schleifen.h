2005fbf2941be340905fba6620a53d59f08b5e2b
robischp <Peter.Robisch@msg.group>
2020-07-28 18:35:21 +0200

    Schleifen Demo: copy_array

--- cpp-basics/Schleifen/Demo/demo-schleifen.h
+++ cpp-basics/Schleifen/Demo/demo-schleifen.h
@@ -1,4 +1,7 @@
 #ifndef DEMOSCHLEIFEN_H
 #define DEMOSCHLEIFEN_H
 
+#include <iostream>
+void copy_array();
+
 #endif // DEMOSCHLEIFEN_H
--- cpp-basics/Schleifen/Demo/main.cpp
+++ cpp-basics/Schleifen/Demo/main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
+#include "demo-schleifen.h"
 
-using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    copy_array();
+}
+
+
+void copy_array(){
+    std::cout << "copy_array";
+    const size_t size {10};
+    int v1[size] = {0, 1, 2 ,3 , 4, 5, 6 , 7 ,8, 9 };
+    int v2[size];
+
+    for (auto i=0; i!= size; ++i){
+        v2[i] = v1[i];
+        // ... v2 ist
+    }
+    std::cout << "... copy_array done: ";
+    for(auto const& value: v2)
+      {
+          std::cout << value << ",";
+      }
+      std::cout << "\n";
 }
