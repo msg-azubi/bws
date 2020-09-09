3e307d468b5b2092ca4f38fd4e3dc2a080211d32
robischp <Peter.Robisch@msg.group>
2020-09-09 22:51:35 +0200

    reviewing hints to task 01, start hints to task 02

--- cpp-basics/Sortieren/SortierAlgoVergleich/doc/exercise-sheet--given-source-code.cpp
+++ cpp-basics/Sortieren/SortierAlgoVergleich/doc/exercise-sheet--given-source-code.cpp
@@ -5,7 +5,9 @@ datentyp* origArray;
 // end::declare-origArray[]
 
 //tag::define-uiHelper-arrayAnzeigen[]
+//tag::use-of-typdef-datentyp-as-parameter-type[]
 void arrayAnzeigen(datentyp *array,int anz, QListWidget* lwAnzeige)
+//end::use-of-typdef-datentyp-as-parameter-type[]
 {
     lwAnzeige->clear();
     lwAnzeige->setVisible(false); //sonst kostet die Anzeige zu viel Rechenzeit!
@@ -17,9 +19,13 @@ void arrayAnzeigen(datentyp *array,int anz, QListWidget* lwAnzeige)
 //end::define-uiHelper-arrayAnzeigen[]
 
 //tag::define-algorithm-bubbleSort[]
+//tag::use-of-typdef-datentyp-as-parameter-type[]
 void bubbleSort(datentyp *feld, int anz) //aufsteigendes Sortieren
+//end::use-of-typdef-datentyp-as-parameter-type[]
 {
+    // tag::use-of-typdef-datentyp-as-variable-type-in-function-bubbleSort[]
     datentyp *tmp;
+    // end::use-of-typdef-datentyp-as-variable-type-in-function-bubbleSort[]
     for (int x=0; x<anz-1; x++) {
         for (int i=0; i<=anz-1; i++) {
             if (feld[i]<feld[i+1]) {
@@ -37,8 +43,12 @@ void FrmMain::on_btnBubbleSort_clicked()
 {
     this->setCursor(Qt::WaitCursor);
     QTime time;
+    //tag::use-of-typdef-datentyp-as-variable-type-in-function-on_btnBubbleSort_clicked[]
     datentyp* tmpArray = new datentyp[anzElemente];
+    //end::use-of-typdef-datentyp-as-variable-type-in-function-on_btnBubbleSort_clicked[]
+    //tag::use-of-variable-origArray[]
     tmpArray = origArray;
+    //end::use-of-variable-origArray[]
     time.start();
     bubbleSort(tmpArray, anzElemente);
     ui->lblZeitBubbleSort->setText(QString::number(time.elapsed())+ " ms");
